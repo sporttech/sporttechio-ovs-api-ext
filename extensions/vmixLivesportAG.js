78b92793ddc1bd0d8e068d074386dd142c007bde
PavelSholukha <pavel@sporttech.biz>
2024-06-27 17:59:09 +0400

    Add results request

--- extensions/vmixLivesportAG.js
+++ extensions/vmixLivesportAG.js
@@ -1,13 +1,4 @@
-const { transformStageList } = require('./vmixLivesportCommon');
-const buffer = require("circular-buffer");
-
-const F_STARTED = 1;
-const F_PUBLISHED = 3;
-const F_STATES = {};
-F_STATES[F_STARTED] = "started";
-F_STATES[F_PUBLISHED] = "published";
-
-const recentFramesInFoucs = new buffer(10);
+const { transformStageList, splitStartListChunks, splitResultsChunks, updateFrameData, bindTeam, bindTeamFlag, updateFramesInFocus, recentFrames } = require('./vmixLivesportCommon');
 
 let M = {};
 
@@ -17,111 +8,8 @@ let config = {
     root: "/vmid/bra/ag"
 };
 
-function bindTeam(a) {
-    if (a.Representing in config.teams) {
-        return config.teams[a.Representing].name;
-    }
-    return a.Representing;
-}
-function bindTeamFlag(a) {
-    if (a.Representing in config.teams && config.teams[a.Representing].flag !== "") {
-        return config.teams[a.Representing].flag;
-    }
-    return `${ OVS }/static/img/assets/named/${ a.Representing }`;
-}
-
-function processFrameUodate(frame) {
-        if (!("State" in frame)) {
-            return
-        }
-        const state = frame.State;
-        if (state in F_STATES) {
-            recentFramesInFoucs.enq({ID: frame.ID, state: F_STATES[state]});
-        }
-}
-
-function updateFramesInFocus(updateM) {
-    if (Object.keys(updateM["Frames"]).length > 10) {
-        // Probably that is initial model load, skipping frames monitoring
-        // and filling in buffer with Panel frames
-        for (const panel of Object.values(updateM["Panels"])) {
-            if (!("FrameID" in panel)) {
-                continue;
-            }
-            const fid = panel.FrameID;
-            if (!(fid in updateM.Frames)) {
-                continue;
-            }
-            processFrameUodate(updateM.Frames[fid]);
-        }
-        return;
-    }
-    
-    for (const frame of Object.values(updateM["Frames"])) {
-        processFrameUodate(frame);
-    }
-}
-
 function onModelUpdated(updateM) {
     updateFramesInFocus(updateM);
-}
-
-
-/// Start list request 
-function newStartListChunk(event, competition, stage, group, groupIdx, chunk) {
-    return {
-		event: event,
-		competition: competition,
-        stage: stage,
-        group: group,
-		chunk: chunk,
-		groupIdx: groupIdx,
-        performances: []
-    };
-}
-
-
-function splitStartListChunks(data, max, sid) {
-    const chunks = [];
-	const event = data.Event;
-    const stage = data.Stages[sid];
-    if (stage === undefined) {
-        return [];
-    }
-	const competition = data.Competitions[stage.CompetitionID];
-
-	for (const [idx, gid] of stage.Groups.entries()) {
-		const group = data.Groups[gid];
-		let chunkCount = 1;
-		let chunk = newStartListChunk(event, competition, stage, group, idx, chunkCount);
-		for (const [pidx, pid] of group.Performances.entries()) {
-			const performance = data.Performances[pid];
-
-			chunk.performances.push({
-				athlete: data.Athletes[performance.Athletes[0]],
-				order: pidx + 1
-			});
-			if (chunk.performances.length >= max && max > 0) {
-				chunks.push(chunk);
-				chunkCount++;
-				chunk = newStartListChunk(event, competition, stage, group, idx, chunkCount);
-			}
-		}
-		chunks.push(chunk);
-	}
-
-    return chunks;
-}
-
-function updateFrameData(frameData, key, performances, get) {
-	const lng = performances.length > 8 ? performances.length : 8;
-	for (let i = 0; i < lng; i++) {
-		if (i < performances.length) {
-			frameData[key + "_n" + (i+1)] = get(performances[i]);
-		} else {
-			frameData[key + "_n" + (i+1)] = "";
-		}
-	}
 }
 
 function proccessStartListChunk(chunk) {
@@ -132,17 +20,33 @@ function proccessStartListChunk(chunk) {
 	};
 	updateFrameData(frameData, "order", chunk.performances, ( p ) => { return String(p.order).padStart(2, "0")});
 	updateFrameData(frameData, "name", chunk.performances, ( p ) => { return p.athlete.Surname + " " + p.athlete.GivenName });
-	updateFrameData(frameData, "repr", chunk.performances, ( p ) => { return bindTeam(p.athlete); });
-	updateFrameData(frameData, "logo", chunk.performances, ( p ) => { return bindTeamFlag(p.athlete); } );
-	frameData["event"] = chunk.event.Title;
-	frameData["eventSubtitle"] = chunk.event.Subtitle;
+	updateFrameData(frameData, "repr", chunk.performances, ( p ) => { return bindTeam(p.athlete, config); });
+	updateFrameData(frameData, "logo", chunk.performances, ( p ) => { return bindTeamFlag(p.athlete, config, OVS); } );
+	frameData.event = chunk.event.Title;
+	frameData.eventSubtitle = chunk.event.Subtitle;
+
+	return frameData;
+}
+function proccessResultsChunk(chunk) {
+	const frameData = {
+		competition: chunk.competition.Title,
+	};
+	updateFrameData(frameData, "rank", chunk.performances, ( p ) => { return String(p.rank).padStart(2, "0")});
+	updateFrameData(frameData, "name", chunk.performances, ( p ) => { return p.athlete.Surname + " " + p.athlete.GivenName });
+	updateFrameData(frameData, "repr", chunk.performances, ( p ) => { return bindTeam(p.athlete, config); });
+	updateFrameData(frameData, "logo", chunk.performances, ( p ) => { return bindTeamFlag(p.athlete, config, OVS); } );
+	updateFrameData(frameData, "score", chunk.performances, ( p ) => { return (p.score / 1000).toFixed(3) });
+	frameData.event = chunk.event.Title;
+	frameData.eventSubtitle = chunk.event.Subtitle;
 
 	return frameData;
 }
 
-
 function onStartLists(s_sids, chunkSize) {
     return transformStageList(s_sids, chunkSize, M, splitStartListChunks, proccessStartListChunk)
+}
+function onResultsLists(s_sids, chunkSize) {
+    return transformStageList(s_sids, chunkSize, M, splitResultsChunks, proccessResultsChunk)
 }
 
 /// Results list request 
@@ -163,10 +67,14 @@ module.exports.register = function(app, model, addUpdateListner) {
     }
 
     app.get(config.root + '/recent-frames', (req, res) => {
-        res.json({ recentFramesInFoucs: recentFramesInFoucs.toarray()});
+        res.json({ recentFramesInFoucs: recentFrames()});
     });
     app.get(config.root + '/startlists/:sids/chunk/:size', (req, res) => {
-        const resp = onStartLists(req.params.sids, req.params.size) 
-        res.json(resp);
+        const data = onStartLists(req.params.sids, req.params.size) 
+        res.json(data);
+    });
+    app.get(config.root + '/results/:sids/chunk/:size', (req, res) => {
+        const data = onResultsLists(req.params.sids, req.params.size) 
+        res.json(data);
     });
 };
--- extensions/vmixLivesportCommon.js
+++ extensions/vmixLivesportCommon.js
@@ -1,3 +1,5 @@
+const buffer = require("circular-buffer");
+
 function transformStageList(s_sids, chunkSize, M, stageChunkFunction, tranformFunction) {
     const chunks = [];
     let max = Number(chunkSize);
@@ -18,6 +20,164 @@ function transformStageList(s_sids, chunkSize, M, stageChunkFunction, tranformFu
     return chunks.map(tranformFunction);
 }
 
+function newStartListChunk(event, competition, stage, group, groupIdx, chunk) {
+    return {
+		event: event,
+		competition: competition,
+        stage: stage,
+        group: group,
+		chunk: chunk,
+		groupIdx: groupIdx,
+        performances: []
+    };
+}
+
+function splitStartListChunks(data, max, sid) {
+    const chunks = [];
+	const event = data.Event;
+    const stage = data.Stages[sid];
+    if (stage === undefined) {
+        return [];
+    }
+	const competition = data.Competitions[stage.CompetitionID];
+
+	for (const [idx, gid] of stage.Groups.entries()) {
+		const group = data.Groups[gid];
+		let chunkCount = 1;
+		let chunk = newStartListChunk(event, competition, stage, group, idx, chunkCount);
+		for (const [pidx, pid] of group.Performances.entries()) {
+			const performance = data.Performances[pid];
+
+			chunk.performances.push({
+				athlete: data.Athletes[performance.Athletes[0]],
+				order: pidx + 1
+			});
+			if (chunk.performances.length >= max && max > 0) {
+				chunks.push(chunk);
+				chunkCount++;
+				chunk = newStartListChunk(event, competition, stage, group, idx, chunkCount);
+			}
+		}
+		chunks.push(chunk);
+	}
+
+    return chunks;
+}
+
+function newResultsChunk(event, competition, stage) {
+    return {
+		event: event,
+		competition: competition,
+        stage: stage,
+        performances: []
+    };
+}
+
+
+function splitResultsChunks(data, max, sid) {
+	const performances = [];
+	const event = data.Event;
+	const stage = data.Stages[sid];
+	const competition = data.Competitions[stage.CompetitionID];
+
+    for (const gid of stage.Groups) {
+        const group = data.Groups[gid];
+		for (const pid of group.Performances) {
+			const performance = data.Performances[pid];
+			performances.push({
+				athlete: data.Athletes[performance.Athletes[0]],
+				rank: performance.Rank_G,
+				score: performance.MarkTTT_G,
+			});
+		}
+    }
+	performances.sort((p1, p2) => {
+		return p1.rank - p2.rank;
+	});
+	const chunks = [];
+	const chunkSize = max;
+	for (let i = 0; i < performances.length; i += chunkSize) {
+	    const chunk = newResultsChunk(event, competition, stage);
+		chunk.performances = performances.slice(i, i + chunkSize);
+    	chunks.push(chunk);
+	}
+    return chunks;
+}
+
+function updateFrameData(frameData, key, performances, get) {
+	const lng = performances.length > 8 ? performances.length : 8;
+	for (let i = 0; i < lng; i++) {
+		if (i < performances.length) {
+			frameData[key + "_n" + (i+1)] = get(performances[i]);
+		} else {
+			frameData[key + "_n" + (i+1)] = "";
+		}
+	}
+}
+
+function bindTeam(a, config) {
+    if (a.Representing in config.teams) {
+        return config.teams[a.Representing].name;
+    }
+    return a.Representing;
+}
+function bindTeamFlag(a, config, OVS) {
+    if (a.Representing in config.teams && config.teams[a.Representing].flag !== "") {
+        return config.teams[a.Representing].flag;
+    }
+    return `${ OVS }/static/img/assets/named/${ a.Representing }`;
+}
+
+const recentFramesInFoucs = new buffer(10);
+const F_STARTED = 1;
+const F_PUBLISHED = 3;
+const F_STATES = {};
+F_STATES[F_STARTED] = "started";
+F_STATES[F_PUBLISHED] = "published";
+function processFrameUodate(frame) {
+        if (!("State" in frame)) {
+            return
+        }
+        const state = frame.State;
+        if (state in F_STATES) {
+            recentFramesInFoucs.enq({ID: frame.ID, state: F_STATES[state]});
+        }
+}
+
+function updateFramesInFocus(updateM) {
+    if (Object.keys(updateM["Frames"]).length > 10) {
+        // Probably that is initial model load, skipping frames monitoring
+        // and filling in buffer with Panel frames
+        for (const panel of Object.values(updateM["Panels"])) {
+            if (!("FrameID" in panel)) {
+                continue;
+            }
+            const fid = panel.FrameID;
+            if (!(fid in updateM.Frames)) {
+                continue;
+            }
+            processFrameUodate(updateM.Frames[fid]);
+        }
+        return;
+    }
+    
+    for (const frame of Object.values(updateM["Frames"])) {
+        processFrameUodate(frame);
+    }
+}
+
+function recentFrames() {
+    return recentFramesInFoucs.toarray();
+}
+
+
 module.exports = {
     transformStageList,
+    splitStartListChunks,
+    splitResultsChunks,
+    updateFrameData,
+    bindTeam,
+    bindTeamFlag,
+    updateFramesInFocus,
+    recentFrames
 };
