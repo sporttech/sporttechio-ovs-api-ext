11adaf92d041596a0bd0473f9d489b6c7b06cd11
PavelSholukha <pavel@sporttech.biz>
2024-06-30 17:48:03 +0400

    livesport/TRA: add last result endpoint

--- extensions/vmixLivesportTRA.js
+++ extensions/vmixLivesportTRA.js
@@ -1,6 +1,7 @@
 import { transformStageList, splitStartListChunks, splitResultsChunks, 
         updateFrameData, bindTeam, bindTeamFlag,
-        recentGroups, loadCommonConfig, registerCommonEndpoints} from './vmixLivesportCommon.js';
+        recentGroups, loadCommonConfig, registerCommonEndpoints,
+        recentFrames, F_STATES, F_PUBLISHED} from './vmixLivesportCommon.js';
 
 let M = {};
 
@@ -102,59 +103,67 @@ function getSameAthletePerformance(pRef, s, M) {
     return null;
 }
 
+function describeFrame(fid, M) {
+        const f = M.Frames[fid];
+        const p = M.Performances[f.PerformanceID];
+        const fidx = p.Frames.indexOf(f.ID);
+        const aid = p.Athletes[0];
+        const a = M.Athletes[aid];
+        const g = M.Groups[p.GroupID];
+        const s = M.Stages[g.StageID];
+        const c = M.Competitions[s.CompetitionID];
+        const prevStage = getPrevStage(s,c,M);
+        const aptID = c.Discipline;
+        const e = M.Event;
+
+        const description = {
+            stageID: s.ID,
+            app: config.apparatus[aptID].name,
+            group: s.Groups.indexOf(g.ID) + 1,
+            routine: "R" + (fidx + 1),
+            state: config.frameState[f.State],
+            name: performancePresent(p, M).view,
+            repr: bindTeam(a, config),
+            scoreTotal: (p.MarkTTT_G / 1000).toFixed(3),
+            scoreRoutine: (f.MarkTTT_G / 1000).toFixed(3),
+            scoreDifficulty: (f.DifficultyT_G / 10).toFixed(1),
+            scoreExecution: (f.ETotalT_G / 10).toFixed(1),
+            scorePenalties: (f.PenaltyT / 10).toFixed(1),
+            rank: p.Rank_G,
+            eventTitle: e.Title,
+            competitionTitle: c.Title,
+            logo: bindTeamFlag(a, config, OVS),
+            appIcon: config.apparatus[aptID].icon,
+            scorePrevRoutine: undefined
+        }
+        // Hack for second routine
+        if (fidx > 0) {
+            const prevFrameID = p.Frames[fidx-1];
+            const prevFrame = M.Frames[prevFrameID];
+            description.scorePrevRoutine = (prevFrame.MarkTTT_G / 1000).toFixed(3);
+        }
+        // Get qualification results when possible
+        const pp = getSameAthletePerformance(p, prevStage, M);
+        if (pp !== null) {
+            description.scoreQ = (pp.MarkTTT_G / 1000).toFixed(3);
+            description.rankQ = pp.Rank_G;
+        }
+        return description
+}
+
 function onActiveGroups() {
     const groups = recentGroups(M);
     const rows = [];
     for (const gid of groups) {
         const g = M.Groups[gid];
         const s = M.Stages[g.StageID];
-        const c = M.Competitions[s.CompetitionID];
-        const e = M.Event;
-        const prevStage = getPrevStage(s,c,M);
         for (const pid of g.Performances) {
             const p = M.Performances[pid];
-            const aid = p.Athletes[0];
-            const a = M.Athletes[aid];
             for (const [fidx, fid] of p.Frames.entries()) {
                 if (fidx >= s.PerfomanceFramesLimit) {
                     break;
                 }
-                const f = M.Frames[fid];
-                const aptID = c.Discipline;
-                const athlete = {
-                    stageID: s.ID,
-                    app: config.apparatus[aptID].name,
-                    group: s.Groups.indexOf(g.ID) + 1,
-                    routine: "R" + (fidx + 1),
-                    state: config.frameState[f.State],
-                    name: performancePresent(p, M).view,
-                    repr: bindTeam(a, config),
-                    scoreTotal: (p.MarkTTT_G / 1000).toFixed(3),
-                    scoreRoutine: (f.MarkTTT_G / 1000).toFixed(3),
-                    scoreDifficulty: (f.DifficultyT_G / 10).toFixed(1),
-                    scoreExecution: (f.ETotalT_G / 10).toFixed(1),
-                    scorePenalties: (f.PenaltyT / 10).toFixed(1),
-                    rank: p.Rank_G,
-                    eventTitle: e.Title,
-                    competitionTitle: c.Title,
-                    logo: bindTeamFlag(a, config, OVS),
-                    appIcon: config.apparatus[aptID].icon,
-                    scorePrevRoutine: undefined
-                }
-                // Hack for second VAULT2 routine
-                if (fidx > 0) {
-                    const prevFrameID = p.Frames[fidx-1];
-                    const prevFrame = M.Frames[prevFrameID];
-                    athlete.scorePrevRoutine = (prevFrame.MarkTTT_G / 1000).toFixed(3);
-                }
-                // Get qualification results when possible
-                const pp = getSameAthletePerformance(p, prevStage, M);
-                if (pp !== null) {
-                    athlete.scoreQ = (pp.MarkTTT_G / 1000).toFixed(3);
-                    athlete.rankQ = pp.Rank_G;
-                }
-
-                rows.push(athlete);
+                rows.push(describeFrame(fid, M));
             }
         }
 
@@ -162,9 +171,26 @@ function onActiveGroups() {
     return rows;
 }
 
+function onLastResult() {
+    const recent = recentFrames();
+    if (recent.length === 0) {
+        return [];
+    }
+    for (const event of recent) {
+        if (event.state == F_STATES[F_PUBLISHED]) {
+           return [describeFrame(event.ID, M)]; 
+        }
+    }
+    return [];
+}
+
 
 export async function register(app, model, addUpdateListner) {
     M = model;
     [OVS, config] = await loadCommonConfig("CONFIG_VMIX_LIVESPORT_TRA_FILE", config);
     registerCommonEndpoints(app, config, addUpdateListner, onStartLists, onResultsLists, onActiveGroups);
+    app.get(config.root + '/last-result', (req, res) => {
+        const data = onLastResult();
+        res.json(data);
+    });
 };
